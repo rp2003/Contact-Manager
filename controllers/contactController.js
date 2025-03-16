1213f804757937dd886d2d3151f69e553e148700
rp2003 <riya2@myseneca.ca>
2025-03-16 00:22:03 -0400

    Implemented Error Handling

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,42 +1,52 @@
+// NOTE: As we are going to use mongodb which returns a promise so we have to use async in all functions. So in async function we need a try catch to handle a error to avoid doing it for all functions. we use middleware i.e. express async handler
+const asyncHandler= require("express-async-handler");
+
 //@desc Get all contacts
 // @route Get /api/contacts
 //@access public
 
-const getContact= (req,res)=>{
+const getContact= asyncHandler(async(req,res)=>{
     res.status(200).json({message: "Get all contacts"});
-} 
+});
 
 //@desc Post contact
 //@route Post /api/contacts
 //@access public
 // status 201 is for created
-const createContact= (req,res)=>{
+const createContact= asyncHandler(async(req,res)=>{
+    console.log("The request Body :",req.body);
+    const {name, email,phone} =req.body;
+    if(!name|| !email|| !phone)
+    {
+        res.status(400);
+        throw new Error("All fields are mandatory.")
+    }
     res.status(201).json({message: "create contacts"});
-}
+});
 
 //@desc Get contact by ID
 //@route Get /ap/contacts/:id
 //@access public
 
-const getContactByID = (req,res)=>{
+const getContactByID = asyncHandler(async (req,res)=>{
     res.status(200).json({message: `get contact by id ${req.params.id}`});
-}
+});
 
 
 //@desc Update contact by ID
 //@route Put /ap/contacts/:id
 //@access public
-const updateContact = (req,res)=>{
+const updateContact = asyncHandler( async (req,res)=>{
     res.status(200).json({message: `update contacts ${req.params.id}`});
-}
+});
 
 
 //@desc Delete contact by ID
 //@route delete /ap/contacts/:id
 //@access public
-const deleteContact = (req,res)=>{
+const deleteContact = asyncHandler(async (req,res)=>{
     res.status(200).json({message: `delete contacts ${req.params.id}`});
-}
+});
 
 
 module.exports={
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express= require("express");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv=require("dotenv").config();
 
 const app=express()
 
 const port= process.env.PORT||5000;
 
+//middleware for handling the json data
+app.use(express.json());    // handles the json data that client gave.
+
 app.use("/api/contacts", require("./routes/contactRoutes"))
+app.use(errorHandler); // imported it
 
 app.listen(port,()=>{
     console.log(`Server is running on ${port}`)
